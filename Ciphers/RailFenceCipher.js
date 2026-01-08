dcdbdc80363c48a75f3319336f4b9facfd9f8a6a
DivyaMadane <madanedivya1204@gmail.com>
2026-01-08 16:01:56 +0530

    Fix code style issues using Prettier

--- Ciphers/RailFenceCipher.js
+++ Ciphers/RailFenceCipher.js
@@ -1,20 +1,20 @@
 function railFenceCipher(text, rails) {
-  if (rails <= 1) return text;
+  if (rails <= 1) return text
 
-  const fence = Array.from({ length: rails }, () => []);
-  let rail = 0;
-  let direction = 1;
+  const fence = Array.from({ length: rails }, () => [])
+  let rail = 0
+  let direction = 1
 
   for (const char of text) {
-    fence[rail].push(char);
-    rail += direction;
+    fence[rail].push(char)
+    rail += direction
 
     if (rail === 0 || rail === rails - 1) {
-      direction *= -1;
+      direction *= -1
     }
   }
 
-  return fence.flat().join('');
+  return fence.flat().join('')
 }
 
-module.exports = railFenceCipher;
+module.exports = railFenceCipher
